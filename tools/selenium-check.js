90ea8f770c383a57d32ca448eac1ed115709d9f1
XOP <wdybih@gmail.com>
2016-06-04 22:03:31 +0200

    server check updated

--- tools/selenium-check.js
+++ tools/selenium-check.js
@@ -1,3 +1,15 @@
 var log = require('log-util');
+var selenium = require('selenium-standalone');
 
 log.warn('Checking Selenium Server...');
+
+// todo: refactor to process status
+selenium.start({}, function (err, child) {
+    if (err) {
+        log.info('Selenium Server is running OK...')
+    } else {
+        child.kill();
+        log.error('Looks like Selenium Server is not running. Exiting...');
+        process.exit(1);
+    }
+});
