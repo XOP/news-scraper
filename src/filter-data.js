830b07419b93c6076772f58fd4e2140ed1a86405
XOP <wdybih@gmail.com>
2016-07-02 19:30:15 +0200

    filter func take 1

--- src/filter-data.js
+++ src/filter-data.js
@@ -1,1 +1,61 @@
-// todo
+import is from 'is';
+
+import log from './utils/log-wrapper.js';
+
+import _intersectionBy from 'lodash.intersectionby';
+import _differenceBy from 'lodash.differenceby';
+import _isEqual from 'lodash.isequal';
+
+const filterData = (newData, currentData) => {
+    if (!currentData) {
+        log.error('No data provided. Check filterData params.');
+        process.exit(1);
+    }
+
+    if (!is.array(currentData) || !is.array(newData)) {
+        log.error('Data type is not Array. Exiting...');
+        process.exit(1);
+    }
+
+    let filteredData = [];
+    let commonData = [];
+
+    log.verbose('Data is being filtered...');
+
+    if (_isEqual(newData, currentData)) {
+        // if objects are identical
+        // there is no data
+        filteredData = null;
+    } else {
+        commonData = _intersectionBy(newData, currentData, 'title');
+
+        if (!commonData.length) {
+            // if there is no common items
+            // all data is new
+            filteredData = newData;
+        } else {
+            // if there is common items by title
+            // they have to be compared by data
+            currentData.forEach(currentItem => {
+                newData.forEach(newItem => {
+                    if (newItem.title === currentItem.title) {
+                        const differentData = _differenceBy(newItem.data, currentItem.data, 'href');
+
+                        log.debug('New data', differentData);
+
+                        filteredData = filteredData.concat({
+                            title: newItem.title,
+                            data: differentData
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    log.verbose('Data has been filtered!');
+
+    return filteredData;
+};
+
+export default filterData;
