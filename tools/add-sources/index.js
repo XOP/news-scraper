0099bc62c4b85dd10817b77bb9066bf2b2994fb6
XOP <wdybih@gmail.com>
2016-07-09 19:56:18 +0200

    writing added sources to file

--- tools/add-sources/index.js
+++ tools/add-sources/index.js
@@ -4,55 +4,22 @@ var log = require('log-util');
 var prompt = require('prompt');
 
 var cfg = require('../../config.js');
+var schema = require('./schema.js')(cfg);
+var writeResult = require('./write-result.js');
 
-// var readFile = require('../../dist/utils/file-ops.js').readFile;
-var writeFile = require('../../dist/utils/file-ops.js').writeFile;
+var newSources = [];
 
-// Regular Expression for URL validation
-// Copyright (c) 2010-2013 Diego Perini (http://www.iport.it)
-// https://gist.github.com/dperini/729294
-var urlRegexp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+var customFilename = 'custom';
+var filePath = function (fileName) {
+    fileName = fileName || customFilename;
 
-var schema = {
-    properties: {
-        title: {
-            type: 'string',
-            message: 'Title of the source',
-            required: true
-        },
-
-        url: {
-            type: 'string',
-            pattern: urlRegexp,
-            message: 'Please enter valid url,\ne.g. "http://www.news.com/articles"',
-            required: true
-        },
-
-        elem: {
-            type: 'string',
-            message: 'Please enter valid CSS selector for the <a>...</a> element,\ne.g. "#content .articles > .post li h3 > a"',
-            required: true
-        },
-
-        limit: {
-            type: 'number',
-            message: 'Please specify maximum number of links to parse.\nThe default is ' + cfg.limit + ', the possible maximum is ' + cfg.absLimit,
-            required: false,
-            conform: function (value) {
-                if (typeof value === 'number') {
-                    return value < cfg.absLimit;
-                } else {
-                    return false;
-                }
-            }
-        }
-    }
+    return {
+        path: path.resolve(__dirname, cfg.source.path, fileName + '.json'),
+        name: fileName
+    };
 };
 
 log.verbose('Starting sources fill dialog...');
-
-var newSources = [];
-var filePath = path.resolve(__dirname, cfg.source.path, 'custom.json');
 
 prompt.start();
 
@@ -89,7 +56,7 @@ var addSourceDialog = function () {
 
         prompt.get({
             properties: {
-                'add another source?': {
+                'add another source? y/n': {
                     type: 'string',
                     required: true,
                     message: 'Add another source? Type "y" or "n"',
@@ -104,21 +71,14 @@ var addSourceDialog = function () {
                 return;
             }
 
-            if (result['add another source?'] === 'y') {
+            if (result['add another source? y/n'] === 'y') {
                 addSourceDialog();
             } else {
                 log.info('Number of sources added: ' + newSources.length);
 
-                writeFile(filePath, JSON.stringify(newSources), 'utf8')
-                    .then(function () {
-                        log.verbose('New sources were added to:');
-                        log.info(filePath);
-                        log.verbose('Sources fill dialog now terminates');
-                    })
-                    .catch(function (err) {
-                        log.error(err);
-                    });
+                writeResult(newSources, filePath());
 
+                // end dialog
                 return false;
             }
         });
