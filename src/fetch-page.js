7ceb31300c28dd3ff4d669ae7f3adf2ab28ca4c3
XOP <wdybih@gmail.com>
2016-08-15 15:21:50 +0200

    change webdriver to nightmare

--- src/fetch-page.js
+++ src/fetch-page.js
@@ -1,32 +1,29 @@
-import is from 'is';
 import Promise from 'bluebird';
-import { remote } from 'webdriverio';
+import Nightmare from 'nightmare';
 
 import log from './utils/log-wrapper.js';
 
-const wdioOptions = {
-    desiredCapabilities: {
-        browserName: 'chrome'
-    }
-};
+const nightmareOptions = {};
 
 const fetchPage = props => {
     log.verbose(`Fetching page content from ${props.url}...`);
     log.debug('props', props);
 
-    const browser = remote(wdioOptions);
+    const nightmare = Nightmare(nightmareOptions); // eslint-disable-line
 
-    return new Promise((resolve) => browser
-        .init()
-        .url(props.url)
-        .getHTML(props.elem, true).then(data => {
+    return new Promise((resolve) => nightmare
+        .goto(props.url)
+        .evaluate((props) => {
+            const nodeData = document.querySelectorAll(props.elem);
+            const stringData = Array.prototype.map.call(nodeData, dataItem => dataItem.outerHTML);
+
+            return stringData;
+        }, props)
+        .end()
+        .then(data => {
             log.verbose(`Fetching page content from ${props.url} done!`);
             log.info(`${props.url}: ${data.length} links scraped`);
             log.debug('data', data);
-
-            if (!is.array(data)) {
-                data = [data];
-            }
 
             props.data = data;
             resolve(props);
@@ -34,13 +31,13 @@ const fetchPage = props => {
             return data;
         })
         .catch(err => {
+            log.warn(`Oops, something happened: ${err}`);
             log.warn(`${props.url}: ${err.message}`);
             log.info(`${props.url}: 0 links scraped`);
 
             props.data = [];
             resolve(props);
         })
-        .end()
     );
 };
 
