1bddb1c62f94414850540bb9b06544ec08fed8b1
XOP <wdybih@gmail.com>
2016-09-20 18:13:34 +0200

    introduce hapi

---
+++ src/server.js
@@ -0,0 +1,58 @@
+import Hapi from 'hapi';
+
+import scraper from './scraper';
+
+import log from './utils/log-wrapper';
+
+import cfg from '../config';
+
+const server = new Hapi.Server();
+
+const mockDirectives = [
+    {
+        url: 'https://www.smashingmagazine.com/',
+        elem: 'article.post',
+        link: 'h2 > a',
+        author: 'h2 + ul li.a a',
+        time: 'h2 + ul li.rd',
+        image: 'figure > a > img',
+        limit: 6
+    }
+];
+
+server.connection({
+    port: 9000,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('<a href="/scraper">Scraper</a>');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/scraper',
+    handler: function (request, reply) {
+        const data = scraper(mockDirectives, cfg)
+            .then(function (data) {
+                return data;
+            })
+            .catch(function (err) {
+                return err;
+            });
+
+        return reply(data);
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    log.info(`Server running at: ${server.info.uri}`);
+});
