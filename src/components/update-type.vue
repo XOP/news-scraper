8f846ea1dd09de0afaa667ae0c9b8a7b5ef5eff6
XOP <wdybih@gmail.com>
2016-12-10 17:54:21 +0100

    update update-type

--- src/components/update-type.vue
+++ src/components/update-type.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="update-type">
-        <input type="range" step="1" min="1" max="3" />
+        <div class="update-type__heading">
+            I read 
+        </div>
+        
+        <div class="update-type__control">
+            <input type="hidden" v-model="typeValue" data-id="update-type-value" />
+            <select class="update-type__select" v-model="selected">
+                <option value="month">Monthly</option>
+                <option value="week">Weekly</option>
+                <option value="day">Daily</option>
+            </select>
+        </div>
     </div>
 </template>
 
@@ -12,7 +23,30 @@
     
         data () {
             return {
-                
+                selected: 'week',
+                types: {
+                    month: {
+                        label: 'Monthly',
+                        value: 10
+                    },
+                    week: {
+                        label: 'Weekly',
+                        value: 3
+                    },
+                    day: {
+                        label: 'Daily',
+                        value: 1
+                    },
+                }
+            }
+        },
+        
+        computed: {
+            typeLabel: function () {
+                return this.types[this.selected]['label'];
+            },
+            typeValue: function () {
+                return this.types[this.selected]['value'];
             }
         },
     
